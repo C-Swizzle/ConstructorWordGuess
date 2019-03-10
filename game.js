e2c298acc2e19cd4c5599cc7c6c267a7af2f5966
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-03-09 18:31:24 -0800

    >

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 const inquirer=require("inquirer");
+
+const Word=require("./letter");
+var wordArray=["something","Going Back to cali","back to the future","laptop"];
+var word= new Word(wordArray[Math.floor(Math.random()*wordArray.length)]);
+console.log(word);
+
+function guess(){
+inquirer.prompt([
+    {message:"Guess a letter",
+name:"userGuess",type:"input"}
+]).then(function(response){
+    if(response.userGuess.length>1){
+        console.log("One letter at a time!");
+        guess();
+    } else{
+    word.checkLetter(response.userGuess);
+    if (word.stillPlaying){
+        guess();
+    } else{
+        return;
+    }
+}
+})
+}
+guess();
--- letter.js
+++ letter.js
@@ -3,37 +3,134 @@ var capsAlphabet=[];
 for (var i=0;i<alphabet.length;i++){
     capsAlphabet.push(alphabet[i].toUpperCase());
 }
+const inquirer=require("inquirer");
+// function obfuscate(str){
+//     var tempArray=[];
+//     for (var i=0;i<str.length;i++){
+//         if(capsAlphabet.indexOf(str[i])===-1&&alphabet.indexOf(str[i])===-1){
+//             tempArray.push(str[i]);
+//         }
+//         else {
+//             tempArray.push("_");
+//         }
+//     }
+//     return tempArray.join(" ");
+// }
+// console.log(obfuscate("hello there"));
+// function checkHidden(hiddenStr,rawStr,keypress){
+//     var tempRawStrFormatArray=[];
+//     var tempHiddenStrFormatArray=[];
+//     for(var i=0;i<rawStr.length;i++){
+//         tempRawStrFormatArray.push(rawStr[i]);
+//     }
+//     tempRawStrFormatArray.join(" ");
+//     for(var i=0;i<hiddenStr.length;i++){
+//         tempHiddenStrFormatArray.push(hiddenStr[i]);
+//     }
+//     for (var i=0;i<tempRawStrFormatArray.length;i++){
+//         if(keypress===tempRawStrFormatArray[i]){
+//             tempHiddenStrFormatArray[i]=tempRawStrFormatArray[i];
+//         }
+//     }
+//     return tempHiddenStrFormatArray.join("");
+// }
 
-function obfuscate(str){
+// var obfuscated=obfuscate("hello there");
+// console.log(checkHidden(obfuscated,"hello there","h"));
+
+
+function WordGuess(phrase){
+    this.word=phrase.toUpperCase();
     var tempArray=[];
-    for (var i=0;i<str.length;i++){
-        if(capsAlphabet.indexOf(str[i])===-1&&alphabet.indexOf(str[i])===-1){
-            tempArray.push(str[i]);
+    this.attemptsRemain=10;
+    this.stillPlaying=true;
+    this.alreadyGuessed=[];
+    for (var i=0;i<phrase.length;i++){
+        if(capsAlphabet.indexOf(phrase[i])===-1&&alphabet.indexOf(phrase[i])===-1){
+            tempArray.push(phrase[i]);
         }
         else {
             tempArray.push("_");
         }
     }
-    return tempArray.join(" ");
-}
-console.log(obfuscate("hello there"));
-function checkHidden(hiddenStr,rawStr,keypress){
-    var tempRawStrFormatArray=[];
-    var tempHiddenStrFormatArray=[];
-    for(var i=0;i<rawStr.length;i++){
-        tempRawStrFormatArray.push(rawStr[i]);
+    this.hidden=tempArray;
+   
+    this.checkLetter=function(letter){
+        console.log("You guessed "+letter.toUpperCase());
+        var temp=false;
+        for (var i=0;i<this.word.length;i++){
+            if(letter.toUpperCase()===this.word[i]){
+                this.hidden[i]=this.word[i];
+                var temp=true;
+            }
+        }
+        this.showSpaces(temp);
+
     }
-    tempRawStrFormatArray.join(" ");
-    for(var i=0;i<hiddenStr.length;i++){
-        tempHiddenStrFormatArray.push(hiddenStr[i]);
-    }
-    for (var i=0;i<tempRawStrFormatArray.length;i++){
-        if(keypress===tempRawStrFormatArray[i]){
-            tempHiddenStrFormatArray[i]=tempRawStrFormatArray[i];
+    this.showSpaces=function(bool){
+        console.log(this.hidden.join(" "));
+        if(bool){
+            this.win();
+        }else{
+            this.lose();
         }
     }
-    return tempHiddenStrFormatArray.join("");
-}
+    this.win=function(){
+        console.log("Attempts Remaining: "+ this.attemptsRemain);
+        this.checkOver();
+    }
+    this.lose=function(){
+        if(this.attemptsRemain===0){
+            // console.log("You lose!");
+            this.exit();
+        }else{
+        this.attemptsRemain--;
+        console.log("Attempts Remaining: "+ this.attemptsRemain);
+        }
+        
+    }
+    this.exit=function(){
+        console.log("Sorry! You've lost!");
+        this.stillPlaying=false;
+    }
+    this.checkOver=function(){
+        if(this.word===this.hidden.join("")){
+        console.log("Congratulations! You've Won!");
+        this.stillPlaying=false;
+        }
+    }
+};
 
-var obfuscated=obfuscate("hello there");
-console.log(checkHidden(obfuscated,"hello there","h"));
+var newWord = new WordGuess("Hello");
+// console.log(newWord);
+// newWord.checkLetter("e");
+// newWord.checkLetter("l");
+// newWord.checkLetter("f");
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("f");
+
+// newWord.checkLetter("o");
+
+// newWord.checkLetter("h");
+module.exports=WordGuess;
