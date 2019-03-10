48a92c9f7f1ee2642a7840c2a87e4076c9a2989b
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-03-09 23:02:53 -0800

    ye

--- game.js
+++ game.js
@@ -5,11 +5,13 @@ for (var i=0;i<alphabet.length;i++){
     capsAlphabet.push(alphabet[i].toUpperCase());
 }
 const Word=require("./letter");
-var wordArray=["something","Going Back to cali","back to the future","laptop"];
+var wordArray=["something","Going Back to cali","back to the future","laptop","infringement","realtor","university",
+"papyrus","greyhound","videogames","computer","mathematics","psychology","gardening","protein"];
 var otherword= new Word(wordArray[Math.floor(Math.random()*wordArray.length)]);
 // console.log(word);
 
 function guess(word){
+    console.log("--------------------------------------------------------------------------");
     console.log(word.hidden.join(" "));
 inquirer.prompt([
     {message:"Guess a letter",
@@ -17,17 +19,28 @@ name:"userGuess",type:"input"}
 ]).then(function(response){
     if(response.userGuess.length>1){
         console.log("One letter at a time!");
+    console.log("--------------------------------------------------------------------------");
+
         // console.log(word.hidden.join(" "));
         guess(word);
     } else if(capsAlphabet.indexOf(response.userGuess)===-1&&alphabet.indexOf(response.userGuess)===-1){
         console.log("Letters only!");
+    console.log("--------------------------------------------------------------------------");
+
         guess(word);
     }
     else{
     word.checkLetter(response.userGuess);
     if (word.stillPlaying){
         guess(word);
+
+
     } else{
+        var temporary=[];
+    for (var i=0;i<word.word.length;i++){
+        temporary.push(word.word[i]);
+    }
+    console.log(temporary.join(" "));
         return;
     }
 }
